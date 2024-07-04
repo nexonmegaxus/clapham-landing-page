c1cc451d72eb93fbba3bcc723ac21525e00427a8
Jossia Elang <81104672+nexonmegaxus@users.noreply.github.com>
2024-07-04 10:24:45 +0700

    feat: Update ID Version & Google Maps

---
+++ components/Forms.vue
@@ -0,0 +1,8 @@
+<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
+<script>
+  hbspt.forms.create({
+    region: "na1",
+    portalId: "21626674",
+    formId: "204a85c0-4d28-41da-8dbe-6567a4022874"
+  });
+</script>
