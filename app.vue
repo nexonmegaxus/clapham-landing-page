fcd83f5cad1fc65a23d939385e40c987e37899b7
Jossia Elang <81104672+nexonmegaxus@users.noreply.github.com>
2024-07-10 16:25:52 +0700

    feat: Add SEO meta (title and description)

--- app.vue
+++ app.vue
@@ -1,8 +1,20 @@
 <template>
-  <NuxtLayout>
-      <NuxtRouteAnnouncer />
-      <NuxtLoadingIndicator />
-      <UNotifications />
-      <NuxtPage />
-  </NuxtLayout>
+    <NuxtLayout>
+        <NuxtRouteAnnouncer />
+        <NuxtLoadingIndicator />
+        <UNotifications />
+        <NuxtPage />
+    </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+    title: 'Coworking Space Clapham: Solusi Ruang Kerja Modern di Medan | Clapham Collective',
+    ogTitle: 'Coworking Space Clapham: Solusi Ruang Kerja Modern di Medan | Clapham Collective',
+    description:
+        'Dapatkan tempat kerja berkualitas dengan fasilitas lengkap di Clapham Collective Medan. Solusi terbaik untuk membantu bisnis Anda tumbuh dan berkembang.',
+    ogDescription:
+        'Dapatkan tempat kerja berkualitas dengan fasilitas lengkap di Clapham Collective Medan. Solusi terbaik untuk membantu bisnis Anda tumbuh dan berkembang.',
+    ogImage: 'https://clapham.workfrom.id/wp-content/uploads/2022/11/cropped-fav-32x32.png',
+});
+</script>
